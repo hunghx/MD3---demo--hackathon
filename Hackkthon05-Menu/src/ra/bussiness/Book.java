8edb9c82e5991f6e2d6db076c9a8cae0db46dcca
buidinhhuan <128038982+buidinhhuan@users.noreply.github.com>
2023-06-14 11:40:50 +0700

    Add files via upload

---
+++ Hackkthon05-Menu/src/ra/bussiness/Book.java
@@ -0,0 +1,114 @@
+package ra.bussiness;
+
+import java.util.Scanner;
+
+public class Book {
+    private static int nextBookId = 1;
+    private int bookId;
+    private String bookName;
+    private String author;
+    private String descriptions;
+    private double importPrice;
+    private double exportPrice;
+    private float interest;
+    private boolean bookStatus;
+
+    public Book() {
+        this.bookId = nextBookId++;
+        this.bookStatus = true;
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDescriptions() {
+        return descriptions;
+    }
+
+    public void setDescriptions(String descriptions) {
+        this.descriptions = descriptions;
+    }
+
+    public double getImportPrice() {
+        return importPrice;
+    }
+
+    public void setImportPrice(double importPrice) {
+        this.importPrice = importPrice;
+    }
+
+    public double getExportPrice() {
+        return exportPrice;
+    }
+
+    public void setExportPrice(double exportPrice) {
+        this.exportPrice = exportPrice;
+    }
+
+    public float getInterest() {
+        return interest;
+    }
+
+    public boolean isBookStatus() {
+        return bookStatus;
+    }
+
+    public void setBookStatus(boolean bookStatus) {
+        this.bookStatus = bookStatus;
+    }
+
+    public void inputData() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập tên sách:");
+        this.bookName = scanner.nextLine();
+        System.out.println("Nhập tác giả:");
+        this.author = scanner.nextLine();
+        System.out.println("Nhập mô tả (ít nhất 10 ký tự):");
+        this.descriptions = scanner.nextLine();
+        while (this.descriptions.length() < 10) {
+            System.out.println("Mô tả phải có ít nhất 10 ký tự. Vui lòng nhập lại mô tả:");
+            this.descriptions = scanner.nextLine();
+        }
+        System.out.println("Nhập giá nhập (lớn hơn 0):");
+        this.importPrice = scanner.nextDouble();
+        while (this.importPrice <= 0) {
+            System.out.println("Giá nhập phải lớn hơn 0. Vui lòng nhập lại giá nhập:");
+            this.importPrice = scanner.nextDouble();
+        }
+        System.out.println("Nhập giá xuất (lớn hơn 1.2 lần giá nhập):");
+        this.exportPrice = scanner.nextDouble();
+        while (this.exportPrice <= 1.2 * this.importPrice) {
+            System.out.println("Giá xuất phải lớn hơn 1.2 lần giá nhập. Vui lòng nhập lại giá xuất:");
+            this.exportPrice = scanner.nextDouble();
+        }
+        this.interest = (float) (this.exportPrice - this.importPrice);
+    }
+
+    public void displayData() {
+        System.out.println("Mã sách: " + bookId);
+        System.out.println("Tên sách: " + bookName);
+        System.out.println("Tác giả: " + author);
+        System.out.println("Mô tả: " + descriptions);
+        System.out.println("Giá nhập: " + importPrice);
+        System.out.println("Giá xuất: " + exportPrice);
+        System.out.println("Lợi nhuận: " + interest);
+        System.out.println("Trạng thái: " + (bookStatus ? "Còn hàng" : "Hết hàng"));
+    }
+}
---
+++ Hackkthon05-Menu/src/ra/run/BookManagement.java
@@ -0,0 +1,188 @@
+package ra.run;
+
+import ra.bussiness.Book;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BookManagement {
+    private static final int MAX_BOOKS = 20;
+    private static Book[] books = new Book[MAX_BOOKS];
+    private static int bookCount = 0;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+
+        do {
+             System.out.println(
+                    "****************JAVA-HACKATHON-05-BASIC-MENU*************** \n" +
+                    "" +
+                    "1. Nhập số lượng sách thêm mới và nhập thông tin cho từng cuốn sách\n" +
+                    "2. Hiển thị thông tin tất cả sách trong thư viện\n" +
+                    "3. Sắp xếp sách theo lợi nhuận tăng dần\n" +
+                    "4. Xóa sách theo mã sách\n" +
+                    "5. Tìm kiếm tương đối sách theo tên sách hoặc mô tả\n" +
+                    "6. Thay đổi thông tin sách theo mã sách\n" +
+                    "7. Thoát \n"+ "hãy nhập  lưa chọn : ");
+             choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    addNewBooks(scanner);
+                    break;
+                case 2:
+                    displayAllBooks();
+                    break;
+                case 3:
+                    sortBooks();
+                    break;
+                case 4:
+                    deleteBook(scanner);
+                    break;
+                case 5:
+                    searchBooks(scanner);
+                    break;
+                case 6:
+                    editBook(scanner);
+                    break;
+                case 7:
+                    System.out.println("Thoát khỏi chương trình.");
+                    break;
+                default:
+                    System.out.println("Lựa chọn của bạn  không hợp lệ. Vui lòng chọn lại　.");
+            }
+        } while (choice != 7);
+    }
+
+    private static void addNewBooks(Scanner scanner) {
+        System.out.print("Nhập số lượng sách cần thêm: ");
+        int numBooks = scanner.nextInt();
+        scanner.nextLine();
+
+        if (bookCount + numBooks > MAX_BOOKS) {
+            System.out.println("Không thể thêm nhiều sách hơn. Đã đạt công suất tối đa.");
+            return;
+        }
+
+        for (int i = 0; i < numBooks; i++) {
+            Book book = new Book();
+            System.out.println("Nhập thông tin cho Sách" + (i + 1) + ":");
+            book.inputData();
+            books[bookCount] = book;
+            bookCount++;
+        }
+
+        System.out.println("Thêm " + numBooks + " sach thành công");
+    }
+
+    private static void displayAllBooks() {
+        if (bookCount == 0) {
+            System.out.println("Không có sách trong thư viện.");
+            return;
+        }
+
+        System.out.println("Danh sách sách trong thư viện:");
+        for (int i = 0; i < bookCount; i++) {
+            System.out.println("------------------");
+            books[i].displayData();
+        }
+    }
+
+    private static void sortBooks() {
+        if (bookCount == 0) {
+            System.out.println("Không có sách trong thư viện.");
+            return;
+        }
+
+        Arrays.sort(books, 0, bookCount, (book1, book2) -> Float.compare(book1.getInterest(), book2.getInterest()));
+
+        System.out.println("Sách được sắp xếp theo lợi nhuận tăng dần:");
+        for (int i = 0; i < bookCount; i++) {
+            System.out.println("------------------");
+            books[i].displayData();
+        }
+    }
+
+    private static void deleteBook(Scanner scanner) {
+        if (bookCount == 0) {
+            System.out.println("không có sach trong thư viên");
+            return;
+        }
+
+        System.out.print("Nhập id sách cần xoá ");
+        int bookId = scanner.nextInt();
+        scanner.nextLine();
+
+        int bookIndex = findBookIndexById(bookId);
+        if (bookIndex == -1) {
+            System.out.println("không tìm thấy sách trong thư viên ");
+            return;
+        }
+
+         for (int i = bookIndex; i < bookCount - 1; i++) {
+            books[i] = books[i + 1];
+        }
+
+        bookCount--;
+        System.out.println(" sách có  id " + bookId + " đã xoá thành công.");
+    }
+
+    private static void searchBooks(Scanner scanner) {
+        if (bookCount == 0) {
+            System.out.println("không có sách trong thư viên");
+            return;
+        }
+
+        System.out.print("Nhập truy vấn tìm kiếm (tên hoặc mô tả): ");
+        String query = scanner.nextLine();
+
+        System.out.println("Kết quả tìm kiếm:\n");
+        boolean found = false;
+        for (int i = 0; i < bookCount; i++) {
+            if (books[i].getBookName().toLowerCase().contains(query.toLowerCase()) ||
+                    books[i].getDescriptions().toLowerCase().contains(query.toLowerCase())) {
+                found = true;
+                System.out.println("------------------");
+                books[i].displayData();
+            }
+        }
+
+        if (!found) {
+            System.out.println("Không có sách phù hợp với truy vấn tìm kiếm.");
+        }
+    }
+
+    private static void editBook(Scanner scanner) {
+        if (bookCount == 0) {
+            System.out.println("không có sách trong thư viên");
+            return;
+        }
+
+        System.out.print("Nhập ID sách để sửa đổi: ");
+        int bookId = scanner.nextInt();
+        scanner.nextLine();
+
+        int bookIndex = findBookIndexById(bookId);
+        if (bookIndex == -1) {
+            System.out.println("sách không tôn tại");
+            return;
+        }
+
+        System.out.println("Sửa đổi cuốn sách với ID " + bookId + ":");
+        books[bookIndex].displayData();
+        System.out.println("Nhập thông tin mới:");
+
+        books[bookIndex].inputData();
+
+        System.out.println("Thông tin sách sửa đổi thành công.");
+    }
+
+    private static int findBookIndexById(int bookId) {
+        for (int i = 0; i < bookCount; i++) {
+            if (books[i].getBookId() == bookId) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
